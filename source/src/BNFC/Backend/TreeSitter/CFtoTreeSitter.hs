5b6337d3f59d53b8df00bc5693296404e3b4c330
rina <k@rina.fyi>
2026-02-21 13:19:23 +1000

    new file

--- source/src/BNFC/Backend/TreeSitter/CFtoTreeSitter.hs
+++ source/src/BNFC/Backend/TreeSitter/CFtoTreeSitter.hs
@@ -13,18 +13,18 @@
 
 module BNFC.Backend.TreeSitter.CFtoTreeSitter where
 
-import BNFC.Abs (Reg (RSeq, RSeqs, RStar, RAny))
+import BNFC.Abs (Reg)
 import BNFC.Backend.TreeSitter.RegToJSReg
+import BNFC.Backend.TreeSitter.MatchesEmpty(fixSentence, fixPointKnownEmpty, KnownEmpty, Optional(..), OptionalSentForm)
 import BNFC.CF
 import BNFC.Lexing (mkRegMultilineComment, mkRegSingleLineComment)
 import BNFC.PrettyPrint
+
 import Prelude hiding ((<>))
 import Control.Applicative ((<|>))
 
-import qualified Data.Either as Either
 import qualified Data.Maybe as Maybe
 import qualified Data.List as List
-import qualified Data.Set as Set
 import qualified Debug.Trace as Trace
 
 -- | Indent one level of 2 spaces
@@ -128,70 +128,6 @@ stringRule =
   defineSymbol "token_String" <+> text "/\"([^'\\\\]|(\\\\[\"'\\\\tnrf]))*\"/" <> ","
 identRule =
   defineSymbol "token_Ident" <+> text "/[a-zA-Z][a-zA-Z\\d_']*/" <> ","
-
--- | Possibly empty non-terminals (Cat) or terminals (String, as token name).
-type KnownEmpty = Set.Set (Either Cat String)
-data Optional = Optional | NonOptional deriving (Eq, Show)
-type OptionalSentForm = [(Optional, Either Cat String)]
-
--- TODO: if we have a rule like X ::= "" | "A", then we need to go Left []
--- for the first one to indicate that it DOES match empty and indicate that
--- there is nothing remaining if empty is removed.
-
-possiblyEmptyRule :: KnownEmpty -> SentForm -> Either [OptionalSentForm] [SentForm]
-possiblyEmptyRule knownEmpty sentence =
-  if sentenceMatchesEmpty then
-    Left (Maybe.mapMaybe removeEmptyMatch (List.tails sentence))
-  else
-    Right [sentence]
-  where
-    sentenceMatchesEmpty = all (`Set.member` knownEmpty) sentence
-
-    removeEmptyMatch (x:xs) = Just ((NonOptional, x) : map (\x -> (Optional, x)) xs)
-    removeEmptyMatch [] = Nothing
-
-appendSentForms :: Either [OptionalSentForm] [SentForm] -> Either [OptionalSentForm] [SentForm] -> Either [OptionalSentForm] [SentForm]
-appendSentForms (Right x) (Right y) = Right (x ++ y)
-appendSentForms x y = Left (toOptional x ++ toOptional y)
-  where
-    toOptional = either id (map sentToOptionalSent)
-
-sentToOptionalSent :: SentForm -> OptionalSentForm
-sentToOptionalSent = map (\x -> (NonOptional, x))
-
--- | Returns whether the given Cat with the given Rules could match the empty
---   string, given the set of currently-known empty things.
-possiblyEmptyCat :: KnownEmpty -> (Cat, [Rule]) -> Either [OptionalSentForm] [SentForm]
-possiblyEmptyCat knownEmpty (cat, rules) =
-  foldr appendSentForms (Right []) $
-    map (possiblyEmptyRule knownEmpty . rhsRule) rules
-
-possiblyEmptyCats :: [(Cat, [Rule])] -> KnownEmpty -> KnownEmpty
-possiblyEmptyCats cats knownEmpty =
-  Set.fromList (map (Left . fst) newEmpties)
-    `Set.union` knownEmpty
-  where
-    newEmpties = filter (Either.isLeft . possiblyEmptyCat knownEmpty) cats
-
-fixPointKnownEmpty :: [(Cat, [Rule])] -> KnownEmpty
-fixPointKnownEmpty cats =
-  case fixPoint of
-    Just knownEmpty -> Trace.traceShowId knownEmpty
-    Nothing -> error "impossible due to fix point iteration"
-  where
-    knownEmptySeq = iterate (possiblyEmptyCats cats) Set.empty
-
-    fixPoint = fst <$> List.find (uncurry (==)) (knownEmptySeq `zip` drop 1 knownEmptySeq)
-
-applyOptionals :: KnownEmpty -> SentForm -> OptionalSentForm
-applyOptionals knownEmpty = map apply
-  where
-    apply x = (if x `Set.member` knownEmpty then Optional else NonOptional, x)
-
-fixSentence :: KnownEmpty -> SentForm -> [OptionalSentForm]
-fixSentence knownEmpty =
-    either id (map (applyOptionals knownEmpty))
-    . possiblyEmptyRule knownEmpty
 
 -- | First print the entrypoint rule, tree-sitter always use the
 --   first rule as entrypoint and does not support multi-entrypoint.
@@ -333,8 +269,6 @@ formatRhs = wrapChoice . map formatSent
 formatSent :: OptionalSentForm -> Doc
 formatSent = wrapSeq . map (\(isOpt, x) -> isOptional isOpt (fmt x))
   where
-    -- justIf p x = if p x then Just x else Nothing
-
     isOptional Optional = wrapFun "optional" False
     isOptional NonOptional = id
 

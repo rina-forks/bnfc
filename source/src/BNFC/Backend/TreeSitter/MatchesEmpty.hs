9e3e7e91059e1b3cf8243b399e108dba2f848039
rina <k@rina.fyi>
2026-02-21 14:14:37 +1000

    MatchesEmpty

--- source/src/BNFC/Backend/TreeSitter/MatchesEmpty.hs
+++ source/src/BNFC/Backend/TreeSitter/MatchesEmpty.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 {-
     BNF Converter: TreeSitter Grammar Generator
     Copyright (C) 2004  Author:  Markus Forsberg, Michael Pellauer,
@@ -12,59 +14,80 @@
 -}
 module BNFC.Backend.TreeSitter.MatchesEmpty where
 
-import BNFC.CF
-import Prelude hiding ((<>))
+import BNFC.CF(SentForm, Cat, Rule, rhsRule)
 
-import qualified Data.Either as Either
 import qualified Data.Maybe as Maybe
 import qualified Data.List as List
 import qualified Data.Set as Set
 import qualified Debug.Trace as Trace
 
+-- | A single non-terminal ('Cat') or terminal token name ('String').
+-- A 'SentForm' is a list of these 'SentSym's.
+type SentSym = Either Cat String
 
 -- | Possibly empty non-terminals (Cat) or terminals (String, as token name).
-type KnownEmpty = Set.Set (Either Cat String)
+type KnownEmpty = Set.Set SentSym
 data Optional = Optional | NonOptional deriving (Eq, Show)
-type OptionalSentForm = [(Optional, Either Cat String)]
+
+type OptionalSentSym = (Optional, SentSym)
+type OptionalSentForm = [OptionalSentSym]
+
+data MatchesEmpty a = MatchesEmpty a | NonEmpty a deriving (Eq, Show, Functor)
+
+matchesEmpty (MatchesEmpty _) = True
+matchesEmpty (NonEmpty _) = False
+
+unMatchesEmpty (MatchesEmpty x) = x
+unMatchesEmpty (NonEmpty x) = x
+
+seqMatchesEmpty :: Semigroup a => MatchesEmpty a -> MatchesEmpty a -> MatchesEmpty a
+seqMatchesEmpty (MatchesEmpty x) (MatchesEmpty y) = MatchesEmpty (x <> y)
+seqMatchesEmpty               x                y  = NonEmpty (unMatchesEmpty x <> unMatchesEmpty y)
+
+seqListMatchesEmpty :: Monoid a => [MatchesEmpty a] -> MatchesEmpty a
+seqListMatchesEmpty = foldr seqMatchesEmpty (MatchesEmpty mempty)
+
+choiceMatchesEmpty :: Semigroup a => MatchesEmpty a -> MatchesEmpty a -> MatchesEmpty a
+choiceMatchesEmpty (NonEmpty x) (NonEmpty y) = NonEmpty (x <> y)
+choiceMatchesEmpty           x            y  = MatchesEmpty (unMatchesEmpty x <> unMatchesEmpty y)
+
+choiceListMatchesEmpty :: Monoid a => [MatchesEmpty a] -> MatchesEmpty a
+choiceListMatchesEmpty = foldr choiceMatchesEmpty (NonEmpty mempty)
 
 -- TODO: if we have a rule like X ::= "" | "A", then we need to go Left []
 -- for the first one to indicate that it DOES match empty and indicate that
 -- there is nothing remaining if empty is removed.
 
-possiblyEmptyRule :: KnownEmpty -> SentForm -> Either [OptionalSentForm] [SentForm]
-possiblyEmptyRule knownEmpty sentence =
-  if sentenceMatchesEmpty then
-    Left (Maybe.mapMaybe removeEmptyMatch (List.tails sentence))
+possiblyEmptySentSym :: KnownEmpty -> SentSym -> MatchesEmpty OptionalSentSym
+possiblyEmptySentSym knownEmpty sym =
+  if sym `Set.member` knownEmpty then
+    MatchesEmpty (Optional, sym)
   else
-    Right [sentence]
+    NonEmpty (NonOptional, sym)
+
+possiblyEmptyRule :: KnownEmpty -> SentForm -> MatchesEmpty [OptionalSentForm]
+possiblyEmptyRule knownEmpty sent =
+  case seqListMatchesEmpty sent' of
+    MatchesEmpty syms -> MatchesEmpty $ Maybe.mapMaybe headNonOptional (List.tails syms)
+    NonEmpty syms -> NonEmpty [syms]
   where
-    sentenceMatchesEmpty = all (`Set.member` knownEmpty) sentence
+    sent' = map (fmap pure . possiblyEmptySentSym knownEmpty) sent
 
-    removeEmptyMatch (x:xs) = Just ((NonOptional, x) : map (\x -> (Optional, x)) xs)
-    removeEmptyMatch [] = Nothing
-
-appendSentForms :: Either [OptionalSentForm] [SentForm] -> Either [OptionalSentForm] [SentForm] -> Either [OptionalSentForm] [SentForm]
-appendSentForms (Right x) (Right y) = Right (x ++ y)
-appendSentForms x y = Left (toOptional x ++ toOptional y)
-  where
-    toOptional = either id (map sentToOptionalSent)
-
-sentToOptionalSent :: SentForm -> OptionalSentForm
-sentToOptionalSent = map (\x -> (NonOptional, x))
+    headNonOptional ((_,x):xs) = Just ((NonOptional, x) : xs)
+    headNonOptional [] = Nothing
 
 -- | Returns whether the given Cat with the given Rules could match the empty
 --   string, given the set of currently-known empty things.
-possiblyEmptyCat :: KnownEmpty -> (Cat, [Rule]) -> Either [OptionalSentForm] [SentForm]
+possiblyEmptyCat :: KnownEmpty -> (Cat, [Rule]) -> MatchesEmpty [OptionalSentForm]
 possiblyEmptyCat knownEmpty (_, rules) =
-  foldr appendSentForms (Right []) $
-    map (possiblyEmptyRule knownEmpty . rhsRule) rules
+  choiceListMatchesEmpty $ map (possiblyEmptyRule knownEmpty . rhsRule) rules
 
 possiblyEmptyCats :: [(Cat, [Rule])] -> KnownEmpty -> KnownEmpty
 possiblyEmptyCats cats knownEmpty =
   Set.fromList (map (Left . fst) newEmpties)
     `Set.union` knownEmpty
   where
-    newEmpties = filter (Either.isLeft . possiblyEmptyCat knownEmpty) cats
+    newEmpties = filter (matchesEmpty . possiblyEmptyCat knownEmpty) cats
 
 fixPointKnownEmpty :: [(Cat, [Rule])] -> KnownEmpty
 fixPointKnownEmpty cats =
@@ -76,13 +99,6 @@ fixPointKnownEmpty cats =
 
     fixPoint = fst <$> List.find (uncurry (==)) (knownEmptySeq `zip` drop 1 knownEmptySeq)
 
-applyOptionals :: KnownEmpty -> SentForm -> OptionalSentForm
-applyOptionals knownEmpty = map apply
-  where
-    apply x = (if x `Set.member` knownEmpty then Optional else NonOptional, x)
-
 fixSentence :: KnownEmpty -> SentForm -> [OptionalSentForm]
-fixSentence knownEmpty =
-    either id (map (applyOptionals knownEmpty))
-    . possiblyEmptyRule knownEmpty
+fixSentence knownEmpty = unMatchesEmpty . possiblyEmptyRule knownEmpty
 
